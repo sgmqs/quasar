1b3c121d2fdd5bbf71b2b9010d142de19c7d871d
Sena Heydari <sheydari@dosomething.org>
2017-05-29 14:40:08 -0400

    Updated to include string query to send to DB.

--- etl-scripts/QuasarCampaignActivityParsing.py
+++ etl-scripts/QuasarCampaignActivityParsing.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+import json
 import logging
 
 import config
-import DSHelper
+import DSHelper as dsh
 # from DSMySQL import BladeMySQL
 from DSRogueWebScraper import RogueScraper
 
@@ -22,37 +23,98 @@ class RogueEtl:
     def __init__(self):
         """Setup Rogue Scraper and MySQL connection."""
         self.rogueExtract = RogueScraper()
+        self.campaign_activity_table = config.CAMPAIGN_ACTIVITY_TABLE
         # rogueLoad = BladeMySQL(config.CAMPAIGN_ACTIVITY_TABLE)
 
 
     def full_backfill(self):
+        """Run or resume full backfill of Campaign Activity from Rogue API."""
         final_page = self.rogueExtract.get_total_pages()
         current_page = 1
         while current_page <= final_page:
             print("Current page is %s." % current_page)
-            page_results = self._transform_records(
+            page_results = self._process_records(
                 self._get_activity_page(current_page))
             current_page += 1
-
 
     def insert_record(self):
         """Put sanitized record into Blade DB."""
         print(type(self._get_extract_page(1,40)))
 
     def _get_activity_page(self, page, limit=40):
+        """Get a paginated response from Rogue API."""
         roguePage = self.rogueExtract.get_activity(page, limit)
         return roguePage
 
-    def _transform_records(self, rogue_page):
-        """Iterate over page of results from Rogue API and return clean list."""
-        parsed_records = []
+    def _process_records(self, rogue_page):
+        """Iterate over page of results ."""
         for i in rogue_page:
             if i['posts']['data'] == []:
-                i['posts']['data'] = {'id': '', 'status': '',
-                                      'media': {'caption': '', 'url': ''},
-                                      'remote_addr': '','post_source': '',
-                                      'created_at': '', 'updated_at': ''}
-                parsed_records.append(i)
+                query = """INSERT INTO {0} SET northstar_id = \"{1}\",
+                           signup_event_id = \"{2}\",
+                           campaign_id = {3},
+                           campaign_run_id = {4},
+                           quantity = {5},
+                           why_participated = \"{6}\",
+                           signup_source = \"{7}\",
+                           signup_created_at = {8},
+                           signup_updated_at = {9},
+                           post_id = {10},
+                           url = {11},
+                           caption = {12},
+                           status = {13},
+                           remote_addr = {14},
+                           post_source = {15},
+                           submission_created_at = {16},
+                           submission_updated_at = {17}
+                           """.format(self.campaign_activity_table,
+                                      dsh.bare_str(i['northstar_id']),
+                                      dsh.bare_str(i['signup_id']),
+                                      dsh.bare_str(i['campaign_id']),
+                                      dsh.bare_str(i['campaign_run_id']),
+                                      dsh.bare_str(i['quantity']),
+                                      dsh.bare_str(i['why_participated']),
+                                      dsh.bare_str(i['signup_source']),
+                                      dsh.bare_str(i['created_at']),
+                                      dsh.bare_str(i['updated_at']),
+                                      None, None, None, None, None, None,
+                                      None, None)
+                print(query)
             else:
-                parsed_records.append(i)
-        return parsed_records
+                for j in i['posts']['data']:
+                    query = """INSERT INTO {0} SET northstar_id = \"{1}\",
+                            signup_event_id = \"{2}\",
+                            campaign_id = {3},
+                            campaign_run_id = {4},
+                            quantity = {5},
+                            why_participated = \"{6}\",
+                            signup_source = \"{7}\",
+                            signup_created_at = {8},
+                            signup_updated_at = {9},
+                            post_id = {10},
+                            url = \"{11}\",
+                            caption = \"{12}\",
+                            status = \"{13}\",
+                            remote_addr = \"{14}\",
+                            post_source = \"{15}\",
+                            submission_created_at = {16},
+                            submission_updated_at = {17}
+                            """.format(self.campaign_activity_table,
+                                       dsh.bare_str(i['northstar_id']),
+                                       dsh.bare_str(i['signup_id']),
+                                       dsh.bare_str(i['campaign_id']),
+                                       dsh.bare_str(i['campaign_run_id']),
+                                       dsh.bare_str(i['quantity']),
+                                       dsh.bare_str(i['why_participated']),
+                                       dsh.bare_str(i['signup_source']),
+                                       dsh.bare_str(i['created_at']),
+                                       dsh.bare_str(i['updated_at']),
+                                       dsh.bare_str(j['id']),
+                                       dsh.bare_str(j['media']['url']),
+                                       dsh.bare_str(j['media']['caption']),
+                                       dsh.bare_str(j['status']),
+                                       dsh.bare_str(j['remote_addr']),
+                                       dsh.bare_str(j['source']),
+                                       dsh.bare_str(j['created_at']),
+                                       dsh.bare_str(j['updated_at']))
+                    print(query)
