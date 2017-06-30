726f7c9673f5bbc498f0d4e9afaad6148f7ac618
xianny <xianny@gmail.com>
2017-06-29 18:00:06 -0700

    Fix indentation

--- quasar/etl/mobile_commons_campaign_scraper.py
+++ quasar/etl/mobile_commons_campaign_scraper.py
@@ -40,33 +40,33 @@ def main():
 
     # Iterate through all Mobile Commons Campaigns and populate "mobile_campaign_id_lookup" table. opt_in_path is the Primary Key to the table.
     for campaign in mob_com_campaigns:
-      print("****************************")
-      name = campaign.find('name')
-      description = campaign.find('description')
-      campaign_id = campaign.get('id')
-      opt_in_path = campaign.find_all('opt_in_path')
-      for path in opt_in_path:
-          path_name = path.find('name')
-          matches = nid_run_id.match(description.text)
-          if matches is not None:
-              nid = matches.group(1)
-              run_id = matches.group(2)
-              if run_id is not None:
-                  insert_campaign = "replace into users_and_activities.mobile_campaign_id_lookup VALUES ({0}, \"{1}\", {2}, \"{3}\", {4}, {5})".format(path.get('id'), path_name.text.replace("\"", ""), campaign_id, name.text.replace("\"", ""), nid, run_id)
-                  print (insert_campaign)
-                  cur.execute(insert_campaign)
-                  db.commit()
-              else:
-                  insert_campaign = "replace into users_and_activities.mobile_campaign_id_lookup VALUES ({0}, \"{1}\", {2}, \"{3}\", {4}, {5})".format(path.get('id'), path_name.text.replace("\"", ""), campaign_id, name.text.replace("\"", ""), nid, 'NULL')
-                  print (insert_campaign)
-                  cur.execute(insert_campaign)
-                  db.commit()
-          else:
-              insert_campaign = "replace into users_and_activities.mobile_campaign_id_lookup VALUES ({0}, \"{1}\", {2}, \"{3}\", {4}, {5})".format(path.get('id'), path_name.text.replace("\"", ""), campaign_id, name.text.replace("\"", ""), 'NULL', 'NULL')
-              print (insert_campaign)
-              cur.execute(insert_campaign)
-              db.commit()
-      print("****************************")
+        print("****************************")
+        name = campaign.find('name')
+        description = campaign.find('description')
+        campaign_id = campaign.get('id')
+        opt_in_path = campaign.find_all('opt_in_path')
+        for path in opt_in_path:
+            path_name = path.find('name')
+            matches = nid_run_id.match(description.text)
+            if matches is not None:
+                nid = matches.group(1)
+                run_id = matches.group(2)
+                if run_id is not None:
+                    insert_campaign = "replace into users_and_activities.mobile_campaign_id_lookup VALUES ({0}, \"{1}\", {2}, \"{3}\", {4}, {5})".format(path.get('id'), path_name.text.replace("\"", ""), campaign_id, name.text.replace("\"", ""), nid, run_id)
+                    print (insert_campaign)
+                    cur.execute(insert_campaign)
+                    db.commit()
+                else:
+                    insert_campaign = "replace into users_and_activities.mobile_campaign_id_lookup VALUES ({0}, \"{1}\", {2}, \"{3}\", {4}, {5})".format(path.get('id'), path_name.text.replace("\"", ""), campaign_id, name.text.replace("\"", ""), nid, 'NULL')
+                    print (insert_campaign)
+                    cur.execute(insert_campaign)
+                    db.commit()
+            else:
+                insert_campaign = "replace into users_and_activities.mobile_campaign_id_lookup VALUES ({0}, \"{1}\", {2}, \"{3}\", {4}, {5})".format(path.get('id'), path_name.text.replace("\"", ""), campaign_id, name.text.replace("\"", ""), 'NULL', 'NULL')
+                print (insert_campaign)
+                cur.execute(insert_campaign)
+                db.commit()
+        print("****************************")
 
     cur.close()
     db.close()
