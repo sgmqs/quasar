4755a42d6e3052f44616ca10e1db4a4ba4ac54fe
xianny <xianny@gmail.com>
2017-07-05 10:38:08 -0700

    Add entrypoint for phoenix db daily snapshot

--- quasar/etl/daily_phoenix_db_snapshot.py
+++ quasar/etl/daily_phoenix_db_snapshot.py
@@ -2,24 +2,28 @@ import MySQLdb
 import time
 from .config import config
 
-db = MySQLdb.connect(host=config.host, #hostname
-          user=config.user, #  username
-          passwd=config.pw, #  password
-          db='phoenix_user_snapshots') # datatype conversions
+def main():
+    db = MySQLdb.connect(host=config.host, #hostname
+              user=config.user, #  username
+              passwd=config.pw, #  password
+              db='phoenix_user_snapshots') # datatype conversions
 
-cur = db.cursor()
+    cur = db.cursor()
 
-snapshot_time = str(int(time.time()))
+    snapshot_time = str(int(time.time()))
 
-#Create Table Schema Snapshot
-run_query = "CREATE TABLE IF NOT EXISTS phoenix_users_snapshot_" + snapshot_time + """ LIKE dosomething.users"""
-cur.execute(run_query)
+    #Create Table Schema Snapshot
+    run_query = "CREATE TABLE IF NOT EXISTS phoenix_users_snapshot_" + snapshot_time + """ LIKE dosomething.users"""
+    cur.execute(run_query)
 
-# Insert Daily Data from DS Users Table
-run_query = "INSERT phoenix_users_snapshot_" + snapshot_time + """ SELECT * FROM dosomething.users"""
-cur.execute(run_query)
-db.commit()
+    # Insert Daily Data from DS Users Table
+    run_query = "INSERT phoenix_users_snapshot_" + snapshot_time + """ SELECT * FROM dosomething.users"""
+    cur.execute(run_query)
+    db.commit()
 
-# Close Cursor and Connection
-cur.close()
-db.close()
+    # Close Cursor and Connection
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
