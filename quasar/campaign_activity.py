6ceb26c19a8138ff0956d3bda3a674a711f16800
xianny <xianny@gmail.com>
2017-07-12 16:01:17 -0700

    Refactor API calls

--- quasar/campaign_activity.py
+++ quasar/campaign_activity.py
@@ -6,15 +6,11 @@ import time
 from .config import config
 from .utils import strip_str
 from .DSMySQL import BladeMySQL
-from .QuasarWebScraper import Scraper
+from .API import API
 
 log_format = "%(asctime)s - %(levelname)s: %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
-
-db = BladeMySQL()
 campaign_activity_table = config.CAMPAIGN_ACTIVITY_TABLE
-api_root = ''.join((config.ROGUE_URI, '/api/v2/activity'))
-scraper = Scraper(api_root)
 
 
 def full_backfill():
@@ -22,28 +18,33 @@ def full_backfill():
 
 
 def backfill_since():
-    _backfill(backfill_hours=sys.argv[1])
+    _backfill(hours=sys.argv[1])
 
 
-def _backfill(backfill_hours=None):
-    final_page = _get_end_page(backfill_hours)
-    current_page = _get_start_page()
+def _backfill(hours=None):
 
-    if backfill_hours is not None:
-        print("Current backfill hours are %s." % backfill_hours)
+    if hours is not None:
+        print("Current backfill hours are %s." % hours)
 
-    print(final_page)
-    print(current_page)
+    # Setup
+    db = BladeMySQL()
+    start_time = strip_str(_now_minus_hours(hours)) # empty string if None
+    rogueAPI = API(''.join((config.ROGUE_URI, '/api/v2/activity')),
+                   headers={'X-DS-Rogue-API-Key': config.DS_ROGUE_API_KEY},
+                   params={'page': 1, 'limit': 40, 'filter[updated_at]': start_time})
+    final_page = rogueAPI.get('')['meta']['pagination']['total_pages']
+    current_page = _get_start_page(db)
+
+    # Main processing loop
     while current_page <= final_page:
         print("Current page: %s of %s" % (current_page, final_page))
-        start_time = _now_minus_hours(backfill_hours)
-        data = _get_data(current_page, start_time)
-        _process_records(data)
+        data = rogueAPI.get('', params={'page': current_page})['data']
+        _process_records(db, data)
         current_page += 1
+        if hours is None:
+            _update_progress(db, current_page)
 
-        if backfill_hours is None:
-            _update_progress(current_page)
-
+    # Cleanup
     try:
         db.create_disconnect()
     except Exception as e:
@@ -51,40 +52,7 @@ def _backfill(backfill_hours=None):
         sys.exit(0)
 
 
-def _get(path, page=1, params={}):
-
-    auth_header = {'X-DS-Rogue-API-Key': config.DS_ROGUE_API_KEY}
-    default_params = {'page': page, 'limit': 40}
-    default_params.update(params)
-
-    response = scraper.get(path, headers=auth_header,
-                           query_params=default_params)
-    return response.json()
-
-
-def _get_data(page=1, from_time=None):
-    """ Args:
-        from_time (str): Date as MM-DD-YYYY HH:MM:SS. If specified, only returns data after this time
-    """
-    params = {}
-    if from_time is not None:
-        params = {'filter[updated_at]': from_time}
-    return _get('', page, params)['data']
-
-
-def _get_pages(page=1, from_time=None):
-    """ Args:
-        from_time (str): Date as MM-DD-YYYY HH:MM:SS. If specified, only returns data after this time
-    """
-    params = {}
-    if from_time is not None:
-        params = {'filter[updated_at]': from_time}
-    res = _get('', page, params)
-    print(res)
-    return res['meta']['pagination']['total_pages']
-
-
-def _get_start_page():
+def _get_start_page(db):
     table = config.ROGUE_PROGRESS_TABLE
     querystr = ''.join(("SELECT counter_value FROM ", config.ROGUE_PROGRESS_TABLE,
                         " WHERE counter_name = 'rogue_backfill_page'"))
@@ -96,12 +64,6 @@ def _get_start_page():
         return int(last_page)
 
 
-def _get_end_page(backfill_hours=None):
-    start_time = _now_minus_hours(backfill_hours)
-    final_page = _get_pages(from_time=start_time)
-    return final_page
-
-
 def _now_minus_hours(hours):
     """Returns time x hours ago"""
     if hours is None:
@@ -111,19 +73,19 @@ def _now_minus_hours(hours):
         return dt.fromtimestamp(start_time).isoformat()
 
 
-def _update_progress(page):
+def _update_progress(db, page):
     db.query_str("REPLACE INTO " + config.ROGUE_PROGRESS_TABLE +
                  " (counter_name, counter_value) VALUES(%s, %s)",
                  ('rogue_backfill_page', page))
 
 
-def _process_records(rogue_page):
+def _process_records(db, rogue_page):
     """Iterate over page of results and load into Blade DB."""
     for i in rogue_page:
         if i['posts']['data'] == []:
             db.query_str("REPLACE INTO " +
-                              campaign_activity_table +
-                              " SET northstar_id = %s,\
+                         campaign_activity_table +
+                         " SET northstar_id = %s,\
                               signup_id = %s,\
                               campaign_id = %s,\
                               campaign_run_id = %s,\
@@ -139,20 +101,20 @@ def _process_records(rogue_page):
                               remote_addr = NULL,\
                               post_source = NULL,\
                               submission_created_at = ''",
-                              (strip_str(i['northstar_id']),
-                               strip_str(i['signup_id']),
-                               strip_str(i['campaign_id']),
-                               strip_str(i['campaign_run_id']),
-                               strip_str(i['quantity']),
-                               strip_str(i['why_participated']),
-                               strip_str(i['signup_source']),
-                               strip_str(i['created_at']),
-                               strip_str(i['updated_at'])))
+                         (strip_str(i['northstar_id']),
+                          strip_str(i['signup_id']),
+                          strip_str(i['campaign_id']),
+                          strip_str(i['campaign_run_id']),
+                          strip_str(i['quantity']),
+                          strip_str(i['why_participated']),
+                          strip_str(i['signup_source']),
+                          strip_str(i['created_at']),
+                          strip_str(i['updated_at'])))
         else:
             for j in i['posts']['data']:
                 db.query_str("REPLACE INTO " +
-                                  campaign_activity_table +
-                                  " SET northstar_id = %s,\
+                             campaign_activity_table +
+                             " SET northstar_id = %s,\
                                   signup_id = %s,\
                                   campaign_id = %s,\
                                   campaign_run_id = %s,\
@@ -169,20 +131,20 @@ def _process_records(rogue_page):
                                   post_source = %s,\
                                   submission_created_at = %s,\
                                   submission_updated_at = %s",
-                                  (strip_str(i['northstar_id']),
-                                   strip_str(i['signup_id']),
-                                   strip_str(i['campaign_id']),
-                                   strip_str(i['campaign_run_id']),
-                                   strip_str(i['quantity']),
-                                   strip_str(i['why_participated']),
-                                   strip_str(i['signup_source']),
-                                   strip_str(i['created_at']),
-                                   strip_str(i['updated_at']),
-                                   strip_str(j['id']),
-                                   strip_str(j['media']['url']),
-                                   strip_str(j['media']['caption']),
-                                   strip_str(j['status']),
-                                   strip_str(j['remote_addr']),
-                                   strip_str(j['source']),
-                                   strip_str(j['created_at']),
-                                   strip_str(j['updated_at'])))
+                             (strip_str(i['northstar_id']),
+                              strip_str(i['signup_id']),
+                              strip_str(i['campaign_id']),
+                              strip_str(i['campaign_run_id']),
+                              strip_str(i['quantity']),
+                              strip_str(i['why_participated']),
+                              strip_str(i['signup_source']),
+                              strip_str(i['created_at']),
+                              strip_str(i['updated_at']),
+                              strip_str(j['id']),
+                              strip_str(j['media']['url']),
+                              strip_str(j['media']['caption']),
+                              strip_str(j['status']),
+                              strip_str(j['remote_addr']),
+                              strip_str(j['source']),
+                              strip_str(j['created_at']),
+                              strip_str(j['updated_at'])))
