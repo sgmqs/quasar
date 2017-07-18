26814abb9ef068d1092159d4d7bb4852171673c7
Sena Heydari <sheydari@dosomething.org>
2017-07-18 13:40:16 -0400

    Importing config properly.

--- refresh-database.py
+++ refresh-database.py
@@ -1,20 +1,19 @@
 import os
 import subprocess
 
-import quasar.config as sql_config
+from quasar.config import config as config
 
 def get_filelist(dirname):
     filelist = os.listdir(dirname)
     return filelist
 
 filelist = get_filelist('./data/sql/migrations/')
-print(sql_config.user)
 
 for file in filelist:
     with open('./data/sql/migrations/%s' % file, 'r') as f:
-        command = ['mysql', '-u%s' % sql_config.user,
-                   '-p%s' % sql_config.pw,
-                   '--host=%s' % sql_config.host,
-                   '--port=%s' % sql_config.port]
+        command = ['mysql', '-u%s' % config.user,
+                   '-p%s' % config.pw,
+                   '--host=%s' % config.host,
+                   '--port=%s' % config.port]
         proc = subprocess.Popen(command, stdin = f)
         stdout, stderr = proc.communicate()
