0139e749e6652480c52042f2a06c766749bc7449
Sena Heydari <sheydari@dosomething.org>
2017-08-28 01:16:27 -0400

    More changes.

--- quasar/moco_scraper.py
+++ quasar/moco_scraper.py
@@ -3,12 +3,9 @@ from bs4 import BeautifulSoup
 from .config import config
 from .database import Database
 from .scraper import Scraper
+from .utils import strip_str
 
 scraper = Scraper('https://secure.mcommons.com', auth=(config.mc_user, config.mc_pw))
-# connector = boto.connect_s3(config.QUASAR_AWS_S3_ID, config.QUASAR_AWS_S3_SECRET)
-# bucket = connector.get_bucket(config.MOCO_ARCHIVE_BUCKET)
-# s3 = boto.s3.key.Key(bucket)
-
 s3 = boto3.client(
     's3',
     aws_access_key_id=config.QUASAR_AWS_S3_ID,
@@ -21,16 +18,33 @@ def _write_file(filename, data):
     s3.put_object(Key=filename, Bucket=config.MOCO_ARCHIVE_BUCKET,
                   Body=bytes(str(data), encoding='utf-8'))
 
-def scrape_profiles(start_page):
-    page_num = start_page
-    while page_num <= 2:
+
+def _get_start_page(db):
+    querystr = ''.join(("SELECT last_page_scraped FROM ", config.MOCO_PROGRESS_TABLE))
+    start_page = strip_str(db.query(querystr))
+    return start_page
+
+def _update_start_page(db, page):
+    
+
+
+def scrape_profiles(start_page=None):
+    db = Database()
+    if start_page is not None:
+        page_num = start_page
+    else:
+        page_num = _get_start_page(db)
+    while > 0:
         profiles = _get_profile(page_num).find_all('profile')
-        for profile in profiles:
-            filename = profile['id'] + '-' + profile.phone_number.string + '.xml'
-            print(filename)
-            _write_file(filename, profile)
-        page_num += 1
+        if profiles is not None:
+            for profile in profiles:
+                filename = profile['id'] + '-' + profile.phone_number.string + '.xml'
+                print(filename)
+                _write_file(filename, profile)
+            page_num += 1
+        else:
+            page_num = -1
 
 
 def start_scrape():
-    scrape_profiles(1)
+    scrape_profiles()
