3bd140665859cce1620d2fdb86cfd80e8208ddbe
Sena Heydari <sheydari@dosomething.org>
2017-09-25 12:17:14 -0400

    Updating MoCo message scraper to track status and campaigns completed.

--- data/sql/migrations/001_schema_quasar_etl_status.sql
+++ data/sql/migrations/001_schema_quasar_etl_status.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: quasar-staging.c9ajz690mens.us-east-1.rds.amazonaws.com (MySQL 5.7.17-log)
+# Host: quasar.c9ajz690mens.us-east-1.rds.amazonaws.com (MySQL 5.7.17-log)
 # Database: quasar_etl_status
-# Generation Time: 2017-07-11 17:32:09 +0000
+# Generation Time: 2017-09-25 15:45:08 +0000
 # ************************************************************
 
 
@@ -19,12 +19,48 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS quasar_etl_status;
-CREATE DATABASE quasar_etl_status;
-USE quasar_etl_status;
+
+# Dump of table moco_campaign_messages_list
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `moco_campaign_messages_list`;
+
+CREATE TABLE `moco_campaign_messages_list` (
+  `campaign_id` int(11) NOT NULL,
+  `campaign_scrape_completed` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`campaign_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table moco_campaign_messages_page
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `moco_campaign_messages_page`;
+
+CREATE TABLE `moco_campaign_messages_page` (
+  `last_page` int(11) NOT NULL,
+  PRIMARY KEY (`last_page`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table moco_profile_scraper_page
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `moco_profile_scraper_page`;
+
+CREATE TABLE `moco_profile_scraper_page` (
+  `last_page_scraped` int(11) NOT NULL,
+  PRIMARY KEY (`last_page_scraped`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 # Dump of table northstar_ingestion
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `northstar_ingestion`;
 
 CREATE TABLE `northstar_ingestion` (
   `counter_name` varchar(32) NOT NULL,
@@ -37,18 +73,9 @@ CREATE TABLE `northstar_ingestion` (
 # Dump of table rogue_ingestion
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `rogue_ingestion`;
+
 CREATE TABLE `rogue_ingestion` (
-  `counter_name` varchar(32) NOT NULL,
-  `counter_value` int(11) DEFAULT NULL,
-  PRIMARY KEY (`counter_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-# Dump of table thor_northstar_ingestion
-# ------------------------------------------------------------
-
-CREATE TABLE `thor_northstar_ingestion` (
   `counter_name` varchar(32) NOT NULL,
   `counter_value` int(11) DEFAULT NULL,
   PRIMARY KEY (`counter_name`)
--- data/sql/migrations/002_schema_users_and_activities.sql
+++ data/sql/migrations/002_schema_users_and_activities.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: quasar-staging.c9ajz690mens.us-east-1.rds.amazonaws.com (MySQL 5.7.17-log)
+# Host: quasar.c9ajz690mens.us-east-1.rds.amazonaws.com (MySQL 5.7.17-log)
 # Database: users_and_activities
-# Generation Time: 2017-07-11 17:34:40 +0000
+# Generation Time: 2017-09-25 15:43:47 +0000
 # ************************************************************
 
 
@@ -19,12 +19,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS users_and_activities;
-CREATE DATABASE users_and_activities;
-USE users_and_activities;
 
 # Dump of table action_gender
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `action_gender`;
 
 CREATE TABLE `action_gender` (
   `name` varchar(255) CHARACTER SET ucs2 NOT NULL,
@@ -37,6 +36,8 @@ CREATE TABLE `action_gender` (
 # Dump of table action_income
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `action_income`;
+
 CREATE TABLE `action_income` (
   `name` varchar(255) CHARACTER SET ucs2 NOT NULL,
   `income_level` varchar(19) DEFAULT NULL,
@@ -47,6 +48,8 @@ CREATE TABLE `action_income` (
 
 # Dump of table active_by_month
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `active_by_month`;
 
 CREATE TABLE `active_by_month` (
   `date` date NOT NULL,
@@ -59,6 +62,8 @@ CREATE TABLE `active_by_month` (
 
 # Dump of table all_traffic
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `all_traffic`;
 
 CREATE TABLE `all_traffic` (
   `nid` int(11) NOT NULL,
@@ -73,6 +78,8 @@ CREATE TABLE `all_traffic` (
 # Dump of table baseline_mobile_age
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `baseline_mobile_age`;
+
 CREATE TABLE `baseline_mobile_age` (
   `age` bigint(21) DEFAULT NULL,
   `count` bigint(21) NOT NULL DEFAULT '0'
@@ -82,6 +89,8 @@ CREATE TABLE `baseline_mobile_age` (
 
 # Dump of table baseline_mobile_gender
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `baseline_mobile_gender`;
 
 CREATE TABLE `baseline_mobile_gender` (
   `gender` varchar(6) DEFAULT NULL,
@@ -93,6 +102,8 @@ CREATE TABLE `baseline_mobile_gender` (
 # Dump of table baseline_mobile_income
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `baseline_mobile_income`;
+
 CREATE TABLE `baseline_mobile_income` (
   `income_level` varchar(19) DEFAULT NULL,
   `count` bigint(21) NOT NULL DEFAULT '0'
@@ -102,6 +113,8 @@ CREATE TABLE `baseline_mobile_income` (
 
 # Dump of table baseline_mobile_race
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `baseline_mobile_race`;
 
 CREATE TABLE `baseline_mobile_race` (
   `race` varchar(1) DEFAULT NULL,
@@ -113,6 +126,8 @@ CREATE TABLE `baseline_mobile_race` (
 # Dump of table baseline_web_age
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `baseline_web_age`;
+
 CREATE TABLE `baseline_web_age` (
   `age` bigint(21) DEFAULT NULL,
   `count` bigint(21) NOT NULL DEFAULT '0'
@@ -122,6 +137,8 @@ CREATE TABLE `baseline_web_age` (
 
 # Dump of table baseline_web_gender
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `baseline_web_gender`;
 
 CREATE TABLE `baseline_web_gender` (
   `gender` varchar(6) DEFAULT NULL,
@@ -133,6 +150,8 @@ CREATE TABLE `baseline_web_gender` (
 # Dump of table baseline_web_income
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `baseline_web_income`;
+
 CREATE TABLE `baseline_web_income` (
   `income_level` varchar(19) DEFAULT NULL,
   `count` bigint(21) NOT NULL DEFAULT '0'
@@ -143,6 +162,8 @@ CREATE TABLE `baseline_web_income` (
 # Dump of table baseline_web_race
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `baseline_web_race`;
+
 CREATE TABLE `baseline_web_race` (
   `race` varchar(1) DEFAULT NULL,
   `count` bigint(21) NOT NULL DEFAULT '0'
@@ -152,6 +173,8 @@ CREATE TABLE `baseline_web_race` (
 
 # Dump of table bod
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bod`;
 
 CREATE TABLE `bod` (
   `date` date DEFAULT NULL,
@@ -183,6 +206,8 @@ CREATE TABLE `bod` (
 # Dump of table campaign_master
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `campaign_master`;
+
 CREATE TABLE `campaign_master` (
   `nid` int(11) NOT NULL,
   `title` varchar(200) DEFAULT NULL,
@@ -196,6 +221,8 @@ CREATE TABLE `campaign_master` (
 # Dump of table cause_gender
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `cause_gender`;
+
 CREATE TABLE `cause_gender` (
   `name` varchar(255) CHARACTER SET ucs2 NOT NULL,
   `gender` varchar(6) DEFAULT NULL,
@@ -207,6 +234,8 @@ CREATE TABLE `cause_gender` (
 # Dump of table cause_income
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `cause_income`;
+
 CREATE TABLE `cause_income` (
   `name` varchar(255) CHARACTER SET ucs2 NOT NULL,
   `income_level` varchar(19) DEFAULT NULL,
@@ -215,8 +244,47 @@ CREATE TABLE `cause_income` (
 
 
 
+# Dump of table competitions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `competitions`;
+
+CREATE TABLE `competitions` (
+  `id` int(11) NOT NULL,
+  `leaderboard_msg_day` varchar(16) DEFAULT NULL,
+  `rules` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `competition_dates_start` datetime DEFAULT NULL,
+  `competition_dates_end` datetime DEFAULT NULL,
+  `contest_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`contest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table contest
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `contest`;
+
+CREATE TABLE `contest` (
+  `id` int(11) NOT NULL,
+  `campaign_id` int(11) NOT NULL,
+  `campaign_run_id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `sender_name` varchar(64) DEFAULT NULL,
+  `sender_email` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`,`campaign_id`,`campaign_run_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
 # Dump of table entrances
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `entrances`;
 
 CREATE TABLE `entrances` (
   `nid` int(11) NOT NULL,
@@ -230,6 +298,8 @@ CREATE TABLE `entrances` (
 
 # Dump of table list_tracking
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `list_tracking`;
 
 CREATE TABLE `list_tracking` (
   `date` date NOT NULL,
@@ -246,6 +316,8 @@ CREATE TABLE `list_tracking` (
 # Dump of table mailchimp_sub
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mailchimp_sub`;
+
 CREATE TABLE `mailchimp_sub` (
   `email_address` varchar(200) NOT NULL,
   `confirm_time` datetime DEFAULT NULL,
@@ -257,6 +329,8 @@ CREATE TABLE `mailchimp_sub` (
 # Dump of table mailchimp_sub_daily
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mailchimp_sub_daily`;
+
 CREATE TABLE `mailchimp_sub_daily` (
   `email_address` varchar(200) NOT NULL,
   `confirm_time` datetime DEFAULT NULL,
@@ -267,6 +341,8 @@ CREATE TABLE `mailchimp_sub_daily` (
 
 # Dump of table mailchimp_unsub
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mailchimp_unsub`;
 
 CREATE TABLE `mailchimp_unsub` (
   `email_address` varchar(200) NOT NULL,
@@ -280,6 +356,8 @@ CREATE TABLE `mailchimp_unsub` (
 # Dump of table mailchimp_unsub_daily
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mailchimp_unsub_daily`;
+
 CREATE TABLE `mailchimp_unsub_daily` (
   `email_address` varchar(200) NOT NULL,
   `confirm_time` datetime DEFAULT NULL,
@@ -289,8 +367,33 @@ CREATE TABLE `mailchimp_unsub_daily` (
 
 
 
+# Dump of table messages
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `contest_id` int(11) NOT NULL,
+  `label` text,
+  `subject` text,
+  `body` text,
+  `signoff` text,
+  `protip` text,
+  `show_images` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `type_name` varchar(32) DEFAULT NULL,
+  `type_key` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`contest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
 # Dump of table mob_sub_test
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mob_sub_test`;
 
 CREATE TABLE `mob_sub_test` (
   `phone_number` varchar(20) NOT NULL,
@@ -309,6 +412,8 @@ CREATE TABLE `mob_sub_test` (
 # Dump of table mobile_campaign_id_lookup
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mobile_campaign_id_lookup`;
+
 CREATE TABLE `mobile_campaign_id_lookup` (
   `opt_in_path_id` bigint(20) NOT NULL,
   `opt_in_path_id_name` varchar(255) DEFAULT NULL,
@@ -324,6 +429,8 @@ CREATE TABLE `mobile_campaign_id_lookup` (
 # Dump of table mobile_campaign_id_lookup_lite
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mobile_campaign_id_lookup_lite`;
+
 CREATE TABLE `mobile_campaign_id_lookup_lite` (
   `mobile_campaign_id` bigint(20) DEFAULT NULL,
   `run_nid` bigint(20) DEFAULT NULL
@@ -333,6 +440,8 @@ CREATE TABLE `mobile_campaign_id_lookup_lite` (
 
 # Dump of table mobile_campaign_ids
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mobile_campaign_ids`;
 
 CREATE TABLE `mobile_campaign_ids` (
   `nid` int(11) NOT NULL,
@@ -345,55 +454,15 @@ CREATE TABLE `mobile_campaign_ids` (
 
 
 
-# Dump of table mobile_master_lookup
-# ------------------------------------------------------------
-
-CREATE TABLE `mobile_master_lookup` (
-  `ms_phone_number` varchar(20) NOT NULL,
-  `ms_campaign_id` int(11) NOT NULL,
-  `ms_opt_in_id` int(11) NOT NULL,
-  `ms_activated_at` datetime DEFAULT NULL,
-  `ms_opted_out_at_campaign` datetime DEFAULT NULL,
-  `ms_web_alpha` tinyint(4) DEFAULT NULL,
-  `ms_first_seen_campaign` tinyint(4) DEFAULT NULL,
-  `ms_been_alpha` tinyint(4) DEFAULT NULL,
-  `mc_opt_in_path_id` bigint(20) DEFAULT NULL,
-  `mc_opt_in_path_id_name` varchar(255) DEFAULT NULL,
-  `mc_mobile_campaign_id` bigint(20) DEFAULT NULL,
-  `mc_nid` bigint(20) DEFAULT NULL,
-  `mc_run_id` bigint(20) DEFAULT NULL,
-  `n_nid` int(10) unsigned DEFAULT NULL,
-  `n_vid` int(10) unsigned DEFAULT NULL,
-  `n_type` varchar(32) CHARACTER SET ucs2 DEFAULT NULL,
-  `n_language` varchar(12) CHARACTER SET ucs2 DEFAULT NULL,
-  `n_title` varchar(255) CHARACTER SET ucs2 DEFAULT NULL,
-  `n_uid` int(11) DEFAULT NULL,
-  `n_status` int(11) DEFAULT NULL,
-  `n_created` int(11) DEFAULT NULL,
-  `n_changed` int(11) DEFAULT NULL,
-  `n_comment` int(11) DEFAULT NULL,
-  `n_promote` int(11) DEFAULT NULL,
-  `n_sticky` int(11) DEFAULT NULL,
-  `n_tnid` int(10) unsigned DEFAULT NULL,
-  `n_translate` int(11) DEFAULT NULL,
-  `n_uuid` varchar(36) CHARACTER SET ucs2 DEFAULT NULL,
-  `mu_phone_number` varchar(256) DEFAULT NULL,
-  `mu_us_phone_number` bigint(20) DEFAULT NULL,
-  `mu_uid` int(11) DEFAULT NULL,
-  `mu_created_at` datetime DEFAULT NULL,
-  `mu_source_type` varchar(255) DEFAULT NULL,
-  `mu_opt_in_path_id` bigint(20) DEFAULT NULL,
-  `mu_status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
 # Dump of table mobile_master_lookup_lite
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mobile_master_lookup_lite`;
 
 CREATE TABLE `mobile_master_lookup_lite` (
   `ms_phone_number` varchar(20) NOT NULL,
   `ms_activated_at` datetime DEFAULT NULL,
+  `ms_opt_in_id` int(11) NOT NULL,
   `mc_run_id` bigint(20) DEFAULT NULL,
   `n_title` varchar(255) CHARACTER SET ucs2,
   `mu_uid` int(11) DEFAULT NULL,
@@ -404,6 +473,8 @@ CREATE TABLE `mobile_master_lookup_lite` (
 
 # Dump of table mobile_subscriptions
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mobile_subscriptions`;
 
 CREATE TABLE `mobile_subscriptions` (
   `phone_number` varchar(20) NOT NULL,
@@ -428,6 +499,8 @@ CREATE TABLE `mobile_subscriptions` (
 # Dump of table mobile_user_lookup
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mobile_user_lookup`;
+
 CREATE TABLE `mobile_user_lookup` (
   `phone_number` varchar(256) NOT NULL,
   `us_phone_number` bigint(20) DEFAULT NULL,
@@ -447,6 +520,8 @@ CREATE TABLE `mobile_user_lookup` (
 
 # Dump of table mobile_users
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mobile_users`;
 
 CREATE TABLE `mobile_users` (
   `phone_number` varchar(20) NOT NULL,
@@ -477,6 +552,8 @@ CREATE TABLE `mobile_users` (
 # Dump of table mobile_users_back
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mobile_users_back`;
+
 CREATE TABLE `mobile_users_back` (
   `phone_number` varchar(20) NOT NULL,
   `first_name` varchar(200) DEFAULT NULL,
@@ -506,6 +583,8 @@ CREATE TABLE `mobile_users_back` (
 # Dump of table mobile_users_backfill
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `mobile_users_backfill`;
+
 CREATE TABLE `mobile_users_backfill` (
   `phone_number` varchar(20) NOT NULL,
   `first_name` varchar(200) DEFAULT NULL,
@@ -534,6 +613,8 @@ CREATE TABLE `mobile_users_backfill` (
 # Dump of table new_by_month
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `new_by_month`;
+
 CREATE TABLE `new_by_month` (
   `date` date NOT NULL,
   `average_new` int(11) DEFAULT NULL,
@@ -545,6 +626,8 @@ CREATE TABLE `new_by_month` (
 
 # Dump of table niche_data_users
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `niche_data_users`;
 
 CREATE TABLE `niche_data_users` (
   `uid` int(11) NOT NULL,
@@ -576,6 +659,8 @@ CREATE TABLE `niche_data_users` (
 # Dump of table reportbacks
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `reportbacks`;
+
 CREATE TABLE `reportbacks` (
   `nid` int(11) DEFAULT NULL,
   `reportbacks` int(11) DEFAULT NULL,
@@ -591,6 +676,8 @@ CREATE TABLE `reportbacks` (
 # Dump of table sources
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `sources`;
+
 CREATE TABLE `sources` (
   `nid` int(11) NOT NULL,
   `site` varchar(200) NOT NULL,
@@ -603,6 +690,8 @@ CREATE TABLE `sources` (
 
 # Dump of table test_mobile_users
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `test_mobile_users`;
 
 CREATE TABLE `test_mobile_users` (
   `phone_number` varchar(20) NOT NULL,
@@ -633,6 +722,8 @@ CREATE TABLE `test_mobile_users` (
 # Dump of table test_web_users
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `test_web_users`;
+
 CREATE TABLE `test_web_users` (
   `uid` int(11) NOT NULL,
   `first_name` varchar(100) DEFAULT NULL,
@@ -653,8 +744,27 @@ CREATE TABLE `test_web_users` (
 
 
 
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` varchar(128) NOT NULL,
+  `contest_id` int(11) NOT NULL,
+  `campaign_id` int(11) NOT NULL,
+  `subscribed` tinyint(1) DEFAULT NULL,
+  `unsubscribed` tinyint(1) DEFAULT NULL,
+  `waiting_room_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`contest_id`,`campaign_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
 # Dump of table verified_by_month
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `verified_by_month`;
 
 CREATE TABLE `verified_by_month` (
   `date` date NOT NULL,
@@ -666,8 +776,28 @@ CREATE TABLE `verified_by_month` (
 
 
 
+# Dump of table waiting_room
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `waiting_room`;
+
+CREATE TABLE `waiting_room` (
+  `id` int(11) NOT NULL,
+  `open` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `contest_id` int(11) NOT NULL,
+  `signup_date_start` datetime DEFAULT NULL,
+  `signup_date_end` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`,`contest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
 # Dump of table web_signups
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `web_signups`;
 
 CREATE TABLE `web_signups` (
   `nid` int(11) DEFAULT NULL,
@@ -683,6 +813,8 @@ CREATE TABLE `web_signups` (
 
 # Dump of table web_users
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `web_users`;
 
 CREATE TABLE `web_users` (
   `uid` int(11) NOT NULL,
@@ -707,6 +839,8 @@ CREATE TABLE `web_users` (
 # Dump of table web_users_back
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `web_users_back`;
+
 CREATE TABLE `web_users_back` (
   `uid` int(11) NOT NULL,
   `first_name` varchar(100) DEFAULT NULL,
@@ -729,6 +863,8 @@ CREATE TABLE `web_users_back` (
 
 # Dump of table web_users_backfill
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `web_users_backfill`;
 
 CREATE TABLE `web_users_backfill` (
   `uid` int(11) NOT NULL,
