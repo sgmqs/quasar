77c8ae81cb2efb8bfa45e61484d8da9dd6ef09b9
Sena Heydari <sheydari@dosomething.org>
2017-10-02 14:05:15 -0400

    Adding refactored queue class.

--- quasar/queue.py
+++ quasar/queue.py
@@ -8,17 +8,16 @@ import time
 import pika
 
 from .config import config
-from .utils import strip_str, QuasarException
+from .utils import QuasarException
 
 log_format = "%(asctime)s - %(levelname)s: %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
 
 
 class QuasarQueue:
-    """This class handles queue tasks for Blink Quasar Customer.io Queue.
+    """Basic queue handling class for Quasar.
 
-    This first class is to MVP our migration to Customer.io using queueing
-    in our ETL pipeline, a first for DS.
+    This class sets up 
 
     Basic setup for now is assuming only connecting to a single queue.
     For future version, re-factoring so this class handles all the queue
@@ -56,68 +55,46 @@ class QuasarQueue:
             self.channel.start_consuming()
             logging.info("{0} consumer started.".format(self.amqp_queue))
         except KeyboardInterrupt:
-            self.channel.stop_consuming()
+            self.stop_consume()
         self.connection.close()
 
 
     def stop_consume(self, tag="quasar_consumer"):
         self.channel.basic_cancel(tag)
 
-
-    def test_consumer(self, tag="test_consumer"):
-        logging.info("Starting {0} test consumer for one message.")
-        self.channel(basic_consume(self.on_test_message, self.amqp_queue,
-                                   consumer_tag=tag))
-        stop_consume(tag)
-
-    def test_on_message(self, channel, method_frame, header_frame, body, process_message):
-        self.channel.basic_publish(self.amqp_exchange, self.amqp_queue,
-                                           self._body_encode(message_data),
-                                           pika.BasicProperties(
-                                               content_type='application/json',
-                                               delivery_mode=2))
-        message_data = self._body_decode(body)
-        print("Decoding one message.")
-        print("Message contents is: {}").format(body)
-        self.channel.basic_ack(method_frame.delivery_tag)
-        print("Message republished to queue.")
-        sys.exit(0)
-
-    @on_message
-    def test_on_message():
-        pub_message(self, message_data)
-
-
         
-    def on_message(self, channel, method_frame, header_frame, body, process_message):
-    def on_message(process_message):
-    """Decorator to handle messages ingested from queue.
+    def on_message(self, channel, method_frame, header_frame, body, output="true"):
+        message_data = self.body_decode(body)
+        if output == "true":
+            print("Message received.")
+            print("Message method_frame is {}".format(method_frame))
+            print("Message header_frame is {}".format(header_frame))
+            print("Message body is {}".format(body))
+        print("Republishing message.")   
+        self.pub_message(message_data)
+        print("Acking message.")
+        self.ack_message(method_frame)
+        print("Original message ack'd.")
+        time.sleep(0.5)
 
-    Body is automatically JSON decoded and message ID is logged.
-    """
-        def parse_message(*args, **kwargs):
-            message_data = self._body_decode(body)
-            logging.info("[Message {0}]: Received."
-                "".format(message_data['meta']['request_id']))
-            process_message(message_data)
-        return parse_message
 
     def ack_message(self, method_frame):
         self.channel.basic_ack(method_frame.delivery_tag)
 
+
     def pub_message(self, message_data):
         self.channel.basic_publish(self.amqp_exchange, self.amqp_queue,
-                                   self._body_encode(message_data),
+                                   self.body_encode(message_data),
                                    pika.BasicProperties(
                                     content_type='application/json',
                                     delivery_mode=2))
 
-    def _body_decode(self, body):
+    def body_decode(self, body):
         message_response = body.decode()
         try:
             return json.loads(message_response)
         except Exception as e:
             raise QuasarException(e)
 
-    def _body_encode(self, message_data):
+    def body_encode(self, message_data):
         return json.dumps(message_data)
